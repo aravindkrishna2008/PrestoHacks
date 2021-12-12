48febd6db8f8bf57e9e8e405b00a1b4ed5ed7515
aappachi <aravindkrishna.nambi@gmail.com>
2021-12-11 16:52:02 -0800

    Added a ton of stuff, Thanks to SPotify API

--- backend/routes/musicRoute.js
+++ backend/routes/musicRoute.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 
-var SpotifyWebApi = require('spotify-web-api-node');
+const SpotifyWebApi = require('spotify-web-api-node');
 scopes = ['user-read-private', 'user-read-email','playlist-modify-public','playlist-modify-private']
 
 require('dotenv').config();
 
-var spotifyApi = new SpotifyWebApi({
+const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_API_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
   redirectUri: process.env.CALLBACK_URL,
@@ -19,7 +19,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', (req,res) => {
-  var html = spotifyApi.createAuthorizeURL(scopes)
+  const html = spotifyApi.createAuthorizeURL(scopes)
   console.log(html)
   res.send(html+"&show_dialog=true")  
 })
@@ -28,7 +28,7 @@ router.get('/callback', async (req,res) => {
   const { code } = req.query;
   console.log(code)
   try {
-    var data = await spotifyApi.authorizationCodeGrant(code)
+    const data = await spotifyApi.authorizationCodeGrant(code)
     const { access_token, refresh_token } = data.body;
     spotifyApi.setAccessToken(access_token);
     spotifyApi.setRefreshToken(refresh_token);
@@ -41,7 +41,7 @@ router.get('/callback', async (req,res) => {
 
 router.get('/userinfo', async (req,res) => {
     try {
-      var result = await spotifyApi.getMe();
+      const result = await spotifyApi.getMe();
       console.log(result.body);
       res.status(200).send(result.body)
     } catch (err) {
@@ -51,7 +51,7 @@ router.get('/userinfo', async (req,res) => {
 
 router.get('/playlists', async (req,res) => {
   try {
-    var result = await spotifyApi.getUserPlaylists();
+    const result = await spotifyApi.getUserPlaylists();
     console.log(result.body);
     res.status(200).send(result.body);
   } catch (err) {
